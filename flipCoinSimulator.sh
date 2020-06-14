612531259a81eda221a924ffb260244c3511f21b
Chandrashekhar0510 <rathodsahul.gmail.com>
2020-06-14 16:12:57 +0530

    UC-2 : show number of times head or tail won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,12 +2,32 @@
 
 echo "Simlulation to displays the winner Heads or Tails"
 
-coin=$(( $RANDOM%2 ))
 
-if [ $coin -eq 0 ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+head_count=0
+tail_count=0
+first=0
+end=1
+diff=$(( end-first+1 ))
 
+while [[ $tail_count -lt 11 && $head_count -lt 11 ]]
+do
+        res=$(( first + $(( $RANDOM%diff )) ))
+        #echo $res
+
+        if [ $res -eq 0 ]
+        then
+                ((head_count++))
+                echo "Head" $head_count
+        else
+                ((tail_count++))
+                echo "Tail" $tail_count
+        fi
+
+        if [ $head_count == 11 ]
+        then
+                echo "Head win"
+        elif [ $tail_count == 11 ]
+        then
+                echo "Tail win"
+        fi
+done
