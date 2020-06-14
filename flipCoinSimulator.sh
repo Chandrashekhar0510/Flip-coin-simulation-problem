f3f3333e795f4f15e8b58566033d619b83da43f2
Chandrashekhar0510 <rathodsahul.gmail.com>
2020-06-14 16:37:35 +0530

    UC-4 : continues till difference is not 2

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,34 +2,45 @@
 
 echo "Simlulation to displays the winner Heads or Tails"
 
+count=1
+function flipcoin()
+{
+	head_count=0
+	tail_count=0
+	first=0
+	end=1
+	diff=$(( end-first+1 ))
 
-head_count=0
-tail_count=0
-first=0
-end=1
-diff=$(( end-first+1 ))
+	while [[ $tail_count -lt 21 && $head_count -lt 21 ]]
+	do
+	        res=$(( first + $(( $RANDOM%diff )) ))
 
-while [[ $tail_count -lt 21 && $head_count -lt 21 ]]
+        	if [ $res -eq 0 ]
+        	then
+                	((head_count++))
+          	      	echo "Head" $head_count
+        	else
+                	((tail_count++))
+                	echo "Tail" $tail_count
+        	fi
+	done
+
+	if [ $head_count == 21 ]
+	then
+		count=$(( $head_count - $tail_count ))
+		echo "Head win by $count points"
+		echo ""
+		echo ""
+	elif [ $tail_count == 21 ]
+	then
+		count=$(( $tail_count - $head_count ))
+        	echo "Tail win by $count points"
+		echo ""
+		echo ""
+	fi
+}
+
+while [ $count -ne 2 ]
 do
-        res=$(( first + $(( $RANDOM%diff )) ))
-
-        if [ $res -eq 0 ]
-        then
-                ((head_count++))
-                echo "Head" $head_count
-        else
-                ((tail_count++))
-                echo "Tail" $tail_count
-        fi
+	flipcoin
 done
-
-if [ $head_count == 21 ]
-then
-	count=$(( $head_count - $tail_count ))
-	echo "Head win by $count points"
-elif [ $tail_count == 21 ]
-then
-	count=$(( $tail_count - $head_count ))
-        echo "Tail win by $count points"
-fi
-
